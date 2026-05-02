420027e1b6652b89e82f514773bd98e4a2a16f73
Nguyen Khang <nguyenkhang290290@gmail.com>
2026-05-03 02:04:33 +0700

    fix: Dam bao bang du lieu cho deadlock luon duoc tao ra trong buoc 1

--- sql/demo/Loi5_Deadlock.sql
+++ sql/demo/Loi5_Deadlock.sql
@@ -2,21 +2,13 @@
 -- DEMO LỖI 5: DEADLOCK (Bế tắc khóa)
 -- Lỗi này CHỈ demo được khi bạn MỞ 2 TAB QUERY SONG SONG!
 -- ============================================================
-USE QuanLyDKHP;
-
-DROP TABLE IF EXISTS Demo_SiSo;
-CREATE TABLE Demo_SiSo (
-    MaHP VARCHAR(10) PRIMARY KEY,
-    SiSo INT DEFAULT 0
-) ENGINE=InnoDB;
-INSERT INTO Demo_SiSo VALUES ('HP001', 30), ('HP002', 45);
 
 SELECT '
 =====================================================
 HƯỚNG DẪN DEMO DEADLOCK (CẦN LÀM CHUẨN TỪNG BƯỚC)
 =====================================================
 Lý do bạn chạy báo xanh là do bạn QUÊN bôi đen chữ START TRANSACTION!
-Nếu không có START TRANSACTION, MySQL sẽ tự động Commit ngay lập tức -> Mất khóa!
+HOẶC do bạn quên tạo bảng dữ liệu!
 
 Hãy mở 2 Tab SQL và làm ĐÚNG thứ tự bôi đen sau:
 ' AS HuongDan;
@@ -24,13 +16,17 @@ Hãy mở 2 Tab SQL và làm ĐÚNG thứ tự bôi đen sau:
 /*
 ================== PHẦN DÀNH CHO TAB 1 ==================
 
--- BƯỚC 1: Bôi đen và chạy CÙNG LÚC 3 dòng dưới đây ở Tab 1
+-- BƯỚC 1: Bôi đen và chạy CÙNG LÚC 8 dòng dưới đây ở Tab 1
 USE QuanLyDKHP;
-START TRANSACTION;
-UPDATE Demo_SiSo SET SiSo = 31 WHERE MaHP = 'HP001';
+DROP TABLE IF EXISTS Demo_Deadlock;
+CREATE TABLE Demo_Deadlock (MaHP VARCHAR(10) PRIMARY KEY, SiSo INT);
+INSERT INTO Demo_Deadlock VALUES ('HP001', 30), ('HP002', 45);
 
--- BƯỚC 3: Quay lại Tab 1, bôi đen và chạy dòng này (Nó sẽ bị treo quay vòng vòng)
-UPDATE Demo_SiSo SET SiSo = 46 WHERE MaHP = 'HP002';
+START TRANSACTION;
+UPDATE Demo_Deadlock SET SiSo = 31 WHERE MaHP = 'HP001';
+
+-- BƯỚC 3: Quay lại Tab 1, bôi đen và chạy 1 dòng này (Nó sẽ bị treo quay vòng vòng)
+UPDATE Demo_Deadlock SET SiSo = 46 WHERE MaHP = 'HP002';
 
 ===========================================================
 */
@@ -41,11 +37,11 @@ UPDATE Demo_SiSo SET SiSo = 46 WHERE MaHP = 'HP002';
 -- BƯỚC 2: Bôi đen và chạy CÙNG LÚC 3 dòng dưới đây ở Tab 2
 USE QuanLyDKHP;
 START TRANSACTION;
-UPDATE Demo_SiSo SET SiSo = 46 WHERE MaHP = 'HP002';
+UPDATE Demo_Deadlock SET SiSo = 46 WHERE MaHP = 'HP002';
 
--- BƯỚC 4: Bôi đen và chạy dòng này ở Tab 2
+-- BƯỚC 4: Bôi đen và chạy 1 dòng này ở Tab 2
 -- NGAY LẬP TỨC TAB 2 SẼ VĂNG LỖI 1213 DEADLOCK MÀU ĐỎ!
-UPDATE Demo_SiSo SET SiSo = 31 WHERE MaHP = 'HP001';
+UPDATE Demo_Deadlock SET SiSo = 31 WHERE MaHP = 'HP001';
 
 ===========================================================
 */
